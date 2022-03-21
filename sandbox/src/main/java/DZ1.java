261a0f326be35a402959e4d71c243205eedbc510
EShustov <smthinyoureyes@hotmail.com>
2022-03-21 18:33:45 +0300

    Исправлено наименование класса Mfp

---
+++ sandbox/src/main/java/DZ1.java
@@ -0,0 +1,10 @@
+public class DZ1 {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(-6, 0);
+        Point p2 = new Point(10, 10);
+
+        System.out.println(p2.distance(p1));
+    }
+
+
---
+++ sandbox/src/main/java/Mfp.java
@@ -0,0 +1,6 @@
+public class Mfp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
