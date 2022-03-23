e084ee0ad33a34c7bc856b4078c0927bb3caa30d
EShustov <smthinyoureyes@hotmail.com>
2022-03-23 13:35:08 +0300

    Исправлена синтаксическая ошибка

--- sandbox/src/main/java/DZ1.java
+++ sandbox/src/main/java/DZ1.java
@@ -1,10 +1,11 @@
 public class DZ1 {
 
     public static void main(String[] args) {
-        Point p1 = new Point(-6, 0);
-        Point p2 = new Point(10, 10);
+        Point p1 = new Point(-45, 10);
+        Point p2 = new Point(333, 16);
 
         System.out.println(p2.distance(p1));
     }
+}
 
 
