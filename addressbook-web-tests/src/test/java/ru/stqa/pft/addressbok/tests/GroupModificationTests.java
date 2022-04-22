8018ed95c01747d2c71c5deff4588e972342b838
EShustov <smthinyoureyes@hotmail.com>
2022-04-22 18:41:14 +0300

    Добавлен выбор группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/tests/GroupModificationTests.java
@@ -16,7 +16,7 @@ public class GroupModificationTests extends TestBase {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));{
             }
         }
-        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().selectGroup(before-1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("testR", "aa", "aa"));
         app.getGroupHelper().submitGroupModification();
