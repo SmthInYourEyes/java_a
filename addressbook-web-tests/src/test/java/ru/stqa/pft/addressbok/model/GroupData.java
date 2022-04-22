569e510022c75ddfe2e915e0e02459d5ce9595f6
EShustov <smthinyoureyes@hotmail.com>
2022-04-22 19:57:53 +0300

    Добавлено сравнение списка групп в тесте удаления

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/model/GroupData.java
@@ -1,4 +1,25 @@
 package ru.stqa.pft.addressbok.model;
 
+import java.util.Objects;
+
 public record GroupData(String name, String header, String footer) {
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
