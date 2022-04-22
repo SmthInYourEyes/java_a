3778175e4c5bf4e23ec46bf1c433b2de1f2caa1b
EShustov <smthinyoureyes@hotmail.com>
2022-04-22 19:34:31 +0300

    Добавлено сравнение кол-ва групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbok.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
 
 
@@ -11,17 +13,17 @@ public class GroupModificationTests extends TestBase {
     public void testGroupModification() {
 
         app.getNavigationHelper().goToGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));{
             }
         }
-        app.getGroupHelper().selectGroup(before-1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size()-1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("testR", "aa", "aa"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
     }
     }
