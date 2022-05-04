44ef24993b81a1b3d1f7d3540410ef4737448836
EShustov <smthinyoureyes@hotmail.com>
2022-05-04 18:58:33 +0300

    добавление новой группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/model/GroupData.java
@@ -1,13 +1,47 @@
 package ru.stqa.pft.addressbok.model;
 
 import java.util.Objects;
+public class GroupData {
+    private int id;
+    private final String name;
+    private final String header;
+    private final String footer;
 
-public record GroupData( String id, String name, String header, String footer) {
 
+
+
+    public  GroupData(String name, String header, String footer){
+        this.id=0;
+        this.name=name;
+        this.header=header;
+        this.footer=footer;
+        }
+
+        public  GroupData( int id, String name, String header, String footer){
+        this.id=id;
+        this.name=name;
+        this.header=header;
+        this.footer=footer;
+        }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     @Override
-    public String name() {
-        return name;
+    public String toString() {
+        return "GroupData{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 
     @Override
@@ -15,7 +49,7 @@ public record GroupData( String id, String name, String header, String footer) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name);
+        return id == groupData.id && Objects.equals(name, groupData.name);
     }
 
     @Override
@@ -23,17 +57,11 @@ public record GroupData( String id, String name, String header, String footer) {
         return Objects.hash(id, name);
     }
 
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                '}';
+    public String getHeader() {
+        return header;
     }
 
-    @Override
-    public String id() {
-        return id;
+    public String getFooter() {
+        return footer;
     }
-
 }
