4ff31d0df46f0932ffb70c5d9d042b8579852732
EShustov <smthinyoureyes@hotmail.com>
2022-05-05 16:12:44 +0300

    Задание 9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/tests/ContactCreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/tests/ContactCreation.java
@@ -1,15 +1,30 @@
 package ru.stqa.pft.addressbok.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbok.model.ContactData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactCreation extends TestBase {
 
 
     @Test
     public void testCreationContact() {
-        app.getContactHelper().createContact(new ContactData(Integer.valueOf("aaa"), "bbb", "а.com", "[none]"));
+        List<ContactData> before = app.getContactHelper().getContactList();
+        ContactData contact = new ContactData(before.get(before.size() - 1).getId(), "bbb", "а.com", "[none]");
+        app.getContactHelper().createContact(contact);
         app.getNavigationHelper().goToHomePage();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size() + 1);
+
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+        before.add(contact);
+        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }
 
