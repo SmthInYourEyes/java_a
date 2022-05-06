26c13b15bdc0bbaa00d2bf34092e58022ff48110
EShustov <smthinyoureyes@hotmail.com>
2022-05-06 12:42:08 +0300

    Лекция 5.5. Повсеместное использование уникальных идентификаторов объектов
    Для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/appmanager/GroupHelper.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbok.model.GroupData;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GroupHelper extends HelperBase {
 
@@ -40,6 +42,10 @@ public class GroupHelper extends HelperBase {
         wd.findElements(By.name("selected[]")).get(index).click();
     }
 
+    private void selectGroupById(int id) { wd.findElement(By.cssSelector("input[value ='" + id +"']")).click();
+
+    }
+
     public void initGroupModification() {
         click(By.name("edit"));
     }
@@ -55,19 +61,23 @@ public class GroupHelper extends HelperBase {
         submitGroupCreation();
         returnToGroupPage();
     }
-    public void modify(int index, GroupData group) {
-        selectGroup(index);
+    public void modify(GroupData group) {
+        selectGroupById(group.getId());
         initGroupModification();
         fillGroupForm(group);
         submitGroupModification();
         returnToGroupPage();
     }
 
-    public void delete(int index) {
-        selectGroup(index);
+
+    public void delete(GroupData group) {
+        selectGroupById(group.getId());
         deleteSelectedGroups();
         returnToGroupPage();
+
     }
+
+
 
     public boolean isThereAGroup() {
         return isElementPresent(By.name("selected[]"));
@@ -89,7 +99,16 @@ public class GroupHelper extends HelperBase {
 
     }
 
-
-
+    public Set<GroupData> all() {
+        Set<GroupData> groups = new HashSet<GroupData>();
+        List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+        for (WebElement element : elements) {
+            String name = element.getText();
+            int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
+            groups.add(new GroupData().withId(id).withName(name));
+        }
+        return groups;
+    }
 }
+
 
