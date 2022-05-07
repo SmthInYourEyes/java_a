9a8ce460e244b835fd8f77303fd4c400d675cd60
EShustov <smthinyoureyes@hotmail.com>
2022-05-07 10:50:10 +0300

    Задание 10. Удаление контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/tests/ContactDeletion.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/tests/ContactDeletion.java
@@ -3,25 +3,26 @@ package ru.stqa.pft.addressbok.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbok.model.ContactData;
-import ru.stqa.pft.addressbok.model.GroupData;
+import ru.stqa.pft.addressbok.model.Contacts;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactDeletion extends TestBase {
 
     @Test()
     public void testContactDeletion() {
-        if (app.contact().list().size()==0) {
+        if (app.contact().list().size() == 0) {
             app.contact().create(new ContactData().withFirstname("ffg").withLastname("ggt").withGroup("[none]"));
-            {}
+            {
+            }
         }
-        Set<ContactData> before = app.contact().all();
+        Contacts before = app.contact().all();
         ContactData deletedContact = before.iterator().next();
         app.contact().delete(deletedContact);
-        Set<ContactData> after = app.contact().all();
+        Contacts after = app.contact().all();
         Assert.assertEquals(after.size(), before.size() - 1);
-        before.remove(deletedContact);
-        Assert.assertEquals(before, after);
+        assertThat(after, equalTo(before.without(deletedContact)));
     }
 
 
