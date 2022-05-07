c3455cc23687a4ab0dadc2c5dfe5206e50f63623
EShustov <smthinyoureyes@hotmail.com>
2022-05-07 12:31:32 +0300

    Лекция 5.8

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/tests/GroupModificationTests.java
@@ -27,8 +27,8 @@ public class GroupModificationTests extends TestBase {
         GroupData group = new GroupData()
                 .withId(modifiedGroup.getId()).withName("test02").withHeader("aa").withFooter("bb");
         app.group().modify(group);
+        assertThat(app.group().count(), equalTo(before.size()));
         Groups after = app.group().all();
-        assertEquals(after.size(), (before.size()));
         assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
     }
 
